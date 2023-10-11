0c4293f2aa7a72fcb593ae2a0e626fe5e4012acf
Mihai Trandafir <Mihai.Trandafir@kdu.ro>
2023-10-11 09:31:05 +0300

    Scraping every page added

--- src/Functions/scrapeFunc.ts
+++ src/Functions/scrapeFunc.ts
@@ -9,16 +9,16 @@ export async function ScrapeWebsite(url: string): Promise<any> {
     console.log(`🤖 Scraping ${url}...`);
     await page.waitForSelector("a");
     const mainBody = await page.$x(`//*[@id="__next"]/main/div/div/div[2]/div`);
-    
+
     const urlSanitized = url.endsWith("/") ? url.slice(0, -1) : url;
 
-    
 
-    
+
+
     let data = [];
     if (mainBody.length > 0) {
-      for (const child of mainBody){
-        const childData : ChildData = {} as ChildData;
+      for (const child of mainBody) {
+        const childData: ChildData = {} as ChildData;
 
         const link = await child.$eval('a', (el) => el.getAttribute('href'));
 
@@ -39,27 +39,58 @@ export async function ScrapeWebsite(url: string): Promise<any> {
           height: imageHeight,
         }
 
-        // get time and date
         const date = await child.$eval('time', (el) => el.getAttribute('datetime') || null);
-        const title = await child.$eval('div > div:nth-child(2) > div > div > a', el => el.textContent?.trim() || null); 
-        const combinedText = await child.$eval('div > div:nth-child(2) > div > div:nth-child(2)', el => el.textContent?.trim() || null);
-        const category = await child.$eval('div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2)', el => el.textContent?.trim() || null);
+        const title = await child.$eval('div > div:nth-child(2) > div > div > a', el => el.textContent?.trim() || null);
         const author = await child.$eval('div > div:nth-child(2) > div:nth-child(3) > div > div > div', el => el.textContent?.trim() || null);
-        
         const shortDescription = await child.$eval('div > div:nth-of-type(2) > div:nth-of-type(2)', el => el.textContent?.trim() || null);
-
-        // Author's Profession
         const authorProfession = await child.$eval('div > div:nth-of-type(3) > div > div:nth-of-type(2)', el => el.textContent?.trim() || null);
+
         childData.link = postlink;
         childData.image = imageData;
         childData.date = date;
         childData.title = title;
         childData.author = author;
-        childData.category = category;
         childData.short_description = shortDescription;
         childData.authorProfession = authorProfession;
-    
+
         data.push(childData);
+
+        // access the link and get the full description
+        const postPage = await browser.newPage();
+        await postPage.goto(postlink);
+        console.log(`🤖 Scraping ${postlink}...`);
+        await postPage.waitForXPath('//*[@id="__next"]/div/div/div/div[2]/div[1]/div[3]');
+
+        let postSections: string[] = [];
+
+        // Extract individual sections based on the structure and append them to the postSections array
+        const subtitles = await postPage.$x("//*[@id='__next']/div/div/div/div[2]/div[1]/div[3]/div[1]");
+        const paragraphs = await postPage.$x("//*[@id='__next']/div/div/div/div[2]/div[1]/div[3]/div[1]/following-sibling::div[not(self::ul)]");
+        const lists = await postPage.$x("//*[@id='__next']/div/div/div/div[2]/div[1]/div[3]/div[1]/following-sibling::ul");
+
+        for (let subtitle of subtitles) {
+          const text = await subtitle.evaluate(el => el.textContent);
+          if (text !== null) {
+            postSections.push(text);
+          }
+        }
+
+        for (let paragraph of paragraphs) {
+          const text = await paragraph.evaluate(el => el.textContent);
+          if (text !== null) {
+            postSections.push(text);
+          }
+        }
+
+        for (let list of lists) {
+          const items = await list.$$eval('li', lis => lis.map(li => li.textContent));
+          postSections.push(items.join('\n'));
+        }
+
+        // Join the sections with "\n" to format the post content
+        const postContent = postSections.join("\n\n");
+
+        childData.post_content = postContent.trim();
       }
       return data;
     } else {
@@ -87,6 +118,6 @@ interface ChildData {
   title: string | null;
   short_description: string | null;
   author: string | null;
-  category: string | null;
   authorProfession: string | null;
+  post_content: string | null;
 }
